f4bb3d75cbc5ce336946a89863a9c55180676121
huangcrazy <3118755207@qq.com>
2026-05-12 11:49:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ with st.sidebar:
     st.header("🔑 配置中心")
     api_key = st.text_input("输入 API Key", type="password")
     base_url = st.text_input("供应商", value="https://api.deepseek.com")
-    model_name = st.selectbox("选择模型", ["deepseek-chat", "deepseek-reasoner"], index=0)
+    model_name = st.selectbox("选择模型", ["deepseek-v4-flash", "deepseek-v4-pro"], index=0)
 
 # ==========================================
 # 3. 文件读取
